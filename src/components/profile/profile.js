78747c4c24ad25411e9da5f08404d6364525a69c
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-10-31 01:39:51 +0500

    dsfs

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Grid, makeStyles, Paper } from "@material-ui/core";
 
+const useStyle = makeStyles(
+    (theme) => (
+        {
+            root : {
+
+            },
+            paper : {
+
+            }
+        }
+    )
+)
 
 export default function Profile()
 {
+    const classes = useStyle();
+
     return(
-        <div>
-            
+        <div className={classes.root}>
+            <Paper className={classes.paper}>
+
+                <Grid container>
+
+                    <Grid item>
+
+                    </Grid>
+                    <Grid item>
+                        
+                    </Grid>
+
+                </Grid>
+
+            </Paper>
         </div>
     )
 }
