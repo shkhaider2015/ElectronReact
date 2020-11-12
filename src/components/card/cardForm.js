2e0ecbaf3574de0befb9474938821d637906d495
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-11-12 12:58:37 +0500

    dsfdfsf

--- src/components/card/cardForm.js
+++ src/components/card/cardForm.js
@@ -1,11 +1,23 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
+import FORMICON from "../../RawData/forms.png";
 
 const useStyle = makeStyles(
     (theme) => (
         {
             root : {
 
+            },
+            myPaper : {
+                width : theme.spacing(40),
+                height : theme.spacing(20),
+                backgroundColor : 'rgb(232, 197, 58)'
+            },
+            myImage : {
+                width : '50%',
+                height : '80%',
+                opacity : 0.3,
+                rotate : '180deg'
             }
         }
     )
@@ -16,7 +28,11 @@ function CardForm()
     const classes = useStyle();
     return(
         <div>
-            <h1>Card View</h1>
+            <Paper elevation={2} className={classes.myPaper} >
+
+                <img alt="" src={FORMICON} className={classes.myImage} />
+
+            </Paper>
         </div>
     )
 
