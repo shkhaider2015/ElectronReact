2b60825c80312776972b8fec2527332f6027fad4
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-12-23 09:36:35 +0500

    hgg

---
+++ src/components/profile/userProfile.js
@@ -0,0 +1,136 @@
+import { makeStyles, TextField, RadioGroup, Paper, Radio, Button, FormControlLabel, Dialog, Avatar } from "@material-ui/core";
+import React, { useState } from "react";
+import './userProfile.css'
+import { ReactComponent as Client_Tempory_Profile } from '../../RawData/Create.svg';
+import { getDateFromMillis } from "../../utility/utils";
+
+const UserProfilee = ({ obj, handleClose, open }) => {
+
+
+    return (
+        <div>
+            <div className="mt-4 page-content page-container" id="page-content">
+                <div className="ml-4 mt-5 padding">
+                    <div className=" row  container-fluid d-flex justify-content-center">
+                        <div className="col-xl-6 col-md-12">
+                            <div className="card shadow-sm user-card-full">
+                                <div className="row m-l-0 m-r-0">
+                                    <div className="col-sm-3 bg-c-lite-green user-profile">
+                                        <div className="card-block text-center text-white">
+                                            <div className="m-b-25"> <Client_Tempory_Profile height="70%" width="70%" className="mt-4 img-radius " style={{ padding: 0 }} /> </div>
+                                            <h6 className="f-w-600">Sohail Khan</h6>
+                                            {/* <p>Web Designer</p> <i className=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16" /> */}
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-9   ">
+                                        <div className="card-block">
+                                            <div className="row ">
+                                                <h6 className="m-b-20 pr-5 p-b-5 b-b-default f-w-600">Information</h6>
+                                                <h6 style={{ marginLeft: "61%", marginTop: "-3%" }} className="  text-right"></h6>
+                                            </div>
+                                            <div className="row">
+                                                <div className="col-sm-7">
+                                                    <p className="m-b-10 f-w-600">Email</p>
+                                                    <h6 className="text-muted f-w-400">Sohailkhan2k22@Gmail.com</h6>
+                                                </div>
+                                                <div className="col-sm-5">
+                                                    <p className="m-b-10 f-w-600">Phone</p>
+                                                    <h6 className="text-muted f-w-400">0301-2211009</h6>
+                                                </div>
+                                            </div>
+                                            <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600"></h6>
+                                            <div className="row">
+                                                <div className="col-sm-7">
+                                                    <p className="m-b-10 f-w-600">Passord</p>
+                                                    <h6 className="text-muted f-w-400">xxxxxx</h6>
+                                                </div>
+                                                <div className="col-sm-5">
+                                                    <p className="m-b-10 f-w-600">CNIC</p>
+                                                    <h6 className="text-muted f-w-400">42301-XXXXXXX-X</h6>
+                                                </div>
+                                            </div>
+                                            <ul className="social-link list-unstyled m-t-40 m-b-10">
+                                                <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title data-original-title="facebook" data-abc="true"><i className="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true" /></a></li>
+                                                <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title data-original-title="twitter" data-abc="true"><i className="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true" /></a></li>
+                                                <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title data-original-title="instagram" data-abc="true"><i className="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true" /></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+
+
+const UserProfile = ({ obj, handleClose, open }) => {
+
+    return (<Dialog
+        open={open}
+        onClose={handleClose}
+        maxWidth="lg"
+    >
+        <div>
+
+            <div className="card shadow-sm user-card-full">
+                <div className="row m-l-0 m-r-0">
+                    <div className="col-sm-3 bg-c-lite-green user-profile">
+                        <div className="card-block text-center text-white">
+                            <div className="m-b-25"> <Avatar alt="name" src={obj['imageURI']} variant="rounded" style={{ height : '80px', width : '80px' }}  /> </div>
+                            <h6 className="f-w-600">{obj['name']}</h6>
+                            {/* <p>Web Designer</p> <i className=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16" /> */}
+                        </div>
+                    </div>
+                    <div className="col-sm-9   ">
+                        <div className="card-block">
+                            <div className="row ">
+                                <h6 className="m-b-20 pr-5 p-b-5 b-b-default f-w-600">Information</h6>
+                                <h6 style={{ marginLeft: "61%", marginTop: "-3%" }} className="  text-right"></h6>
+                            </div>
+                            <div className="row">
+                                <div className="col-sm-7">
+                                    <p className="m-b-10 f-w-600">Email</p>
+                                    <h6 className="text-muted f-w-400"> {obj['email']} </h6>
+                                </div>
+                                <div className="col-sm-5">
+                                    <p className="m-b-10 f-w-600">Phone</p>
+                                    <h6 className="text-muted f-w-400"> {obj['cellPhone']} </h6>
+                                </div>
+                            </div>
+                            <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600"></h6>
+                            <div className="row">
+                                <div className="col-sm-7">
+                                    <p className="m-b-10 f-w-600">Created At</p>
+                                    <h6 className="text-muted f-w-400"> {getDateFromMillis(obj['createdAt'])} </h6>
+                                </div>
+                                <div className="col-sm-5">
+                                    <p className="m-b-10 f-w-600">CNIC</p>
+                                    <h6 className="text-muted f-w-400">{obj['cnic']}</h6>
+                                </div>
+                            </div>
+                            <ul className="social-link list-unstyled m-t-40 m-b-10">
+                                <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title data-original-title="facebook" data-abc="true"><i className="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true" /></a></li>
+                                <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title data-original-title="twitter" data-abc="true"><i className="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true" /></a></li>
+                                <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title data-original-title="instagram" data-abc="true"><i className="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true" /></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+    </Dialog>)
+}
+
+export default UserProfile;
--- src/components/userlist/userList.js
+++ src/components/userlist/userList.js
@@ -9,6 +9,7 @@ import { AuthContext } from "../../context/authContext";
 import { Link, useNavigate } from "react-router-dom";
 import DialogueBox from "../printable/DialogueBox";
 import { UserListContext } from "../../context/dataContext";
+import  UserProfile from "../profile/userProfile";
 
 const UserList = (props) => {
 
@@ -17,6 +18,8 @@ const UserList = (props) => {
     const users = React.useContext(UserListContext)
 
     const [userList, setUserList] = React.useState([])
+    const [open, setOpen] = React.useState(false)
+    const [propsObject, setPropsObject] = React.useState(null)
 
     const getAllUsers = () => {
         db.collection('users').get()
@@ -65,6 +68,16 @@ const UserList = (props) => {
         .catch((e) => console.error("Error deleteding user : ", e))
     }
 
+    const handleOpen = (obj) =>
+    {
+        console.log("Tr Clicked ....")
+        setOpen(true)
+        setPropsObject(obj)
+    }
+    const handleClose = () =>
+    {
+        setOpen(false)
+    }
 
 
     return (
@@ -73,41 +86,50 @@ const UserList = (props) => {
 
             <div className="logo-hover text-center mt-2 pt-2"><img src={zainlogo} height="13%" width="13%" alt="hjh" onClick={() => navigate(-1)} /></div>
 
-            <table className="table card-body mt-4 ">
-                {/* <caption className="logo-hover text-center mt-2 pt-2"><img src={zainlogo} height="13%" width="13%" alt="hjh" /></caption> */}
-                <tbody>
-                    {users[0].length !== 0 ? users[0].map(
-                        (obj, ind) => (
-                            obj['isDeleted']
-                            ? null
-                            : <tr className="table-hover1  mt-5 pt-5 shadow rounded" key={ind} >
-
-                            <td colSpan="4" className="pt-4 pl-5" >
-                                <div style={{ display: 'flex' }} >
-                                    <Avatar atl="ss" src={obj['imageURI']} />
-                                    <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '3%' }}>
-                                        <span style={{ fontWeight: 'bold' }} > {obj['name']} </span>
-                                        <span style={{ opacity: 0.8 }} > {obj['email']} </span>
+            {
+                open
+                ? <UserProfile obj={propsObject} handleClose={handleClose} open={open} />
+                :  
+                <table className="table card-body mt-4 ">
+                    {/* <caption className="logo-hover text-center mt-2 pt-2"><img src={zainlogo} height="13%" width="13%" alt="hjh" /></caption> */}
+                    <tbody>
+                        {users[0].length !== 0 ? users[0].map(
+                            (obj, ind) => (
+                                obj['isDeleted']
+                                ? null
+                                : <tr className="table-hover1  mt-5 pt-5 shadow rounded" key={ind}  >
+    
+                                <td colSpan="4" className="pt-4 pl-5" onClick={() => handleOpen(obj)} >
+                                    <div style={{ display: 'flex' }} >
+                                        <Avatar atl="ss" src={obj['imageURI']} />
+                                        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '3%' }}>
+                                            <span style={{ fontWeight: 'bold' }} > {obj['name']} </span>
+                                            <span style={{ opacity: 0.8 }} > {obj['email']} </span>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                            <td className="pl-5 pt-3 text-right" colSpan="1">
-                                {
-                                    obj['isAccepted']
-                                        ? <button className="btn text-right btn-succes disable " disabled >Approved</button>
-                                        : <button className="btn text-right btn-info" onClick={() => approve(obj)} >Approve</button>
-                                }
-
-                            </td>
-                            <td className="text-center  pt-3" colSpan="1">
-                               <button className="btn btn-danger" onClick={() => deleteUser(obj)} >Delete</button> 
-                             </td>
-                        </tr>
-                        )
-                    ) : null}
-
-                </tbody>
-            </table>
+                                </td>
+                                <td className="pl-5 pt-3 text-right" colSpan="1">
+                                    {
+                                        obj['isAccepted']
+                                            ? <button className="btn text-right btn-succes disable " disabled >Approved</button>
+                                            : <button className="btn text-right btn-info" onClick={() => approve(obj)} >Approve</button>
+                                    }
+    
+                                </td>
+                                <td className="text-center  pt-3" colSpan="1">
+                                   <button className="btn btn-danger" onClick={() => deleteUser(obj)} >Delete</button> 
+                                 </td>
+                            </tr>
+                            )
+                        ) : null}
+    
+                    </tbody>
+                </table>
+            
+            
+            }
+           
+        
         </div>
     )
 
